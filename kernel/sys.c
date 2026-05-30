63267e2212509f08da0811b74528320a23130258
vergilantte <xvergilantte@gmail.com>
2026-05-30 13:19:33 +0700

    syscall: Bump BPF override to 5.10.239
    Signed-off-by: vergilantte <xvergilantte@gmail.com>

--- kernel/sys.c
+++ kernel/sys.c
@@ -1195,7 +1195,7 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 		!strncmp(current->comm, "netbpfload", 10) ||
 	    	!strncmp(current->comm, "netd", 4) ||
 	    	!strncmp(current->comm, "uprobestats", 11))) {
-		strcpy(tmp.release, "5.4.186");
+		strcpy(tmp.release, "5.10.239");
 		pr_info("fake uname: %s/%d release=%s\n",
 			 current->comm, current->pid, tmp.release);
 	}
